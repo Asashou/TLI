361b3b6cb37c47c6eac8b18827ae546f4bf69ed5
Asashou <aicha.haji@campus.lmu.de>
2022-02-07 09:55:56 +0100

    updated

--- pipeline/scripts/pipeline_220125.sh
+++ pipeline/scripts/pipeline_220125.sh
@@ -8,4 +8,4 @@
 module load anaconda3
 source activate preprocessing
  
-logsave /home/mpg08/aicha.hajiali/TLI_project/TLI_data/preprocessed/2021/211104/steps/postshift/output.log python /home/mpg08/aicha.hajiali/TLI_project/preprocessing/pipeline/scripts/general_pipeline_4D.py /home/mpg08/aicha.hajiali/TLI_project/TLI_data/preprocessed/2021/211104/steps/postshift/general_pipeline_info_1.txt
+logsave /home/mpg08/aicha.hajiali/TLI_project/TLI_data/preprocessed/test_run/output.log python /home/mpg08/aicha.hajiali/TLI_project/preprocessing/pipeline/scripts/general_pipeline_4D.py /home/mpg08/aicha.hajiali/TLI_project/TLI_data/preprocessed/test_run/general_pipeline_info.txt
